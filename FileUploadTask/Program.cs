1ccddef39db2de3070e002ff14082db76a50f070
DGeorgina <dimitrievska1@yahoo.com>
2026-03-05 10:36:03 +0100

    dynamic paths

--- FileUploadTask/Program.cs
+++ FileUploadTask/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Graph.Models;
 using System.Linq;
+using System.IO;
 
 namespace FileUploadTask
 {
@@ -16,24 +17,44 @@ namespace FileUploadTask
 
 
             var drive = await graphClient.Me.Drive.GetAsync();
-            var userDriveId = drive!.Id!;
+            var userDriveId = drive.Id;
+
+            string projectRoot = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.FullName;
+            string localFilePath = Path.Combine(AppContext.BaseDirectory, "fileForUpload.txt");
+            string downloadFilePath = Path.Combine(AppContext.BaseDirectory, "DownloadedFile.txt");
+
+
 
             DriveItem folder = await FileManagementService.CreateOrGetFolder(graphClient, userDriveId, "Myname");
 
             if (folder != null)
             {
-                await FileManagementService.UploadFile(graphClient, userDriveId, folder.Id!, "/Users/georgina/Desktop/FileUploadTask/FileUploadTask/fileForUpload.txt");
+                await FileManagementService.UploadFile(graphClient, userDriveId, folder.Id!, localFilePath);
+
+            }
+            else
+            {
+                Console.WriteLine("The folder is null");
+                return;
+
             }
 
-            var downloadFileRes = await FileManagementService.DownloadFile(graphClient, userDriveId, folder.Id, "fileForUpload.txt", "/Users/georgina/Downloads/fileForUpload.txt");
+            var downloadFileRes = await FileManagementService.DownloadFile(graphClient, userDriveId, folder.Id, "fileForUpload.txt", downloadFilePath);
 
-            var sha1 = FileManagementService.ComputeSHA256("/Users/georgina/Desktop/FileUploadTask/FileUploadTask/fileForUpload.txt");
-            var sha2 = FileManagementService.ComputeSHA256("/Users/georgina/Downloads/fileForUpload.txt");
+
+            var sha1 = FileManagementService.ComputeSHA256(localFilePath);
+            var sha2 = FileManagementService.ComputeSHA256(downloadFilePath);
 
             if (sha1.SequenceEqual(sha2))
             {
                 Console.WriteLine("The hash codes are the same");
             }
+            else
+            {
+                Console.WriteLine("The hash codes are NOT the same");
+            }
+
+
         }
     }
 }
